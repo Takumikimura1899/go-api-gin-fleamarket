2dadf98b64aac787859c3e221f72b2dbe7059143
Takumikimura1899 <kimura141899@gmail.com>
2024-04-06 20:01:51 +0900

    endpoint /items へリクエストを送った時にFindAllメソッドが実行されるように実装

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-fleamarket/controller"
+	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	items := []models.Item{
+		{ID: 1, Name: "Item 1", Price: 100, Description: "This is item 1", SoldOut: false},
+		{ID: 2, Name: "Item 2", Price: 200, Description: "This is item 2", SoldOut: true},
+		{ID: 3, Name: "Item 3", Price: 300, Description: "This is item 3", SoldOut: false},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controller.NewItemController(itemService)
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/items", itemController.FindAll)
 	r.Run()
 }
