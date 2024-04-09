dff3459cfe998c342771fd278636cd457d8881d9
Takumikimura1899 <kimura141899@gmail.com>
2024-04-09 20:52:15 +0900

    repository層 メモリを使用していたものをdbを使用するように変更中 Updateメソッド実装 dtoのjsonタグのsoldOutが間違っていたため修正

--- repositories/item_repository.go
+++ repositories/item_repository.go
@@ -108,7 +108,10 @@ func (r *ItemRepository) FindById(itemId uint) (*models.Item, error) {
 	return &item, nil
 }
 
-// Update implements IItemRepository.
-func (*ItemRepository) Update(updatedItem models.Item) (*models.Item, error) {
-	panic("unimplemented")
+func (r *ItemRepository) Update(updatedItem models.Item) (*models.Item, error) {
+	result := r.db.Save(&updatedItem)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &updatedItem, nil
 }
