6011c0c9b1d2a84075de4514c2454072da09e0b5
Takumikimura1899 <kimura141899@gmail.com>
2024-04-11 08:43:42 +0900

    user登録作成完了 controllerとルーティング

--- main.go
+++ main.go
@@ -17,12 +17,20 @@ func main() {
 	itemService := services.NewItemService(itemRepository)
 	itemController := controller.NewItemController(itemService)
 
+	authRepository := repositories.NewAuthRepository(db)
+	authService := services.NewAuthService(authRepository)
+	authController := controller.NewAuthController(authService)
+
 	r := gin.Default()
 	itemRouter := r.Group("/items")
+	authRouter := r.Group("/auth")
+
 	itemRouter.GET("", itemController.FindAll)
 	itemRouter.GET("/:id", itemController.FindById)
 	itemRouter.POST("", itemController.Create)
 	itemRouter.PUT("/:id", itemController.Update)
 	itemRouter.DELETE("/:id", itemController.Delete)
+
+	authRouter.POST("/signup", authController.SignUp)
 	r.Run()
 }
