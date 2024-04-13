c66c33f639c70ebfe8bec3f32523df668a4aabee
Takumikimura1899 <kimura141899@gmail.com>
2024-04-13 22:41:27 +0900

    FindAllのテスト作成と実行

--- main_test.go
+++ main_test.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"gin-fleamarket/infra"
 	"gin-fleamarket/models"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
 )
 
@@ -50,4 +54,23 @@ func setup() *gin.Engine {
 	router := setUpRouter(db)
 
 	return router
+}
+
+func TestFindAll(t *testing.T) {
+	// テストのセットアップ
+	router := setup()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/items", nil)
+
+	// APIリクエストの実行
+	router.ServeHTTP(w, req)
+
+	// APIの実行結果を取得
+	var res map[string][]models.Item
+	json.Unmarshal(w.Body.Bytes(), &res)
+
+	// アサーション
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, 3, len(res["data"]))
 }
